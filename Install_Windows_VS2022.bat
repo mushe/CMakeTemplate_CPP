411a2e7182c4f1a967bb44dd6b0de7c22fa20171
mushe <mushegithub@gmail.com>
2023-11-03 12:43:10 +0900

    Auto add .gitignore (VS2022)

--- Install_Windows_VS2022.bat
+++ Install_Windows_VS2022.bat
@@ -1,5 +1,6 @@
 mkdir Windows_VS2022
 copy Gitignore_Templates/VisualStudio.gitignore Windows_VS2022
 cd Windows_VS2022
+rename VisualStudio.gitignore .gitignore
 cmake .. -G "Visual Studio 17 2022"
 cmake --build .
